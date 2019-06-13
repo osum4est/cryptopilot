1ccf81b1ed19a5132ca5c898140cfbcb1f53af39
Forrest Jones <fjones-dev@8bitforest.com>
2019-06-12 20:58:51 -0600

    :sparkles: Add currency types to database

--- crypto_trader/models.py
+++ crypto_trader/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
+class Currency(models.Model):
+    currency_id = models.CharField(max_length=16, unique=True, null=False)
+    base_currency = models.CharField(max_length=16)
+    quote_currency = models.CharField(max_length=16)
+
+
 class Candle(models.Model):
+    currency_id = models.CharField(max_length=16, unique=True, null=False)
     granularity = models.IntegerField()
     time = models.BigIntegerField()
     low = models.DecimalField(max_digits=12, decimal_places=4)
