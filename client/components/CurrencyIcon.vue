eaf95d1ff4c61714e3ff8fb430e0ad87fe51245f
Forrest Jones <fjones-dev@8bitforest.com>
2019-06-22 13:26:31 -0600

    :sparkles: Add CurrencyIcon component

---
+++ client/components/CurrencyIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-img :src="getIconPath" :height="75"
+         contain></v-img>
+</template>
+
+<script lang="ts">
+    import {Prop} from "vue-property-decorator";
+    import Component from "vue-class-component";
+    import Vue from "vue";
+
+    @Component
+    export default class CurrencyIcon extends Vue {
+        @Prop()
+        currency!: string;
+
+        get getIconPath() {
+            return require('cryptocurrency-icons/svg/color/' + this.currency.base_currency.toLowerCase() + '.svg')
+        }
+    }
+</script>
